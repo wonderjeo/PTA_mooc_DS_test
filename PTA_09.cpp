6604bf34d1f545b1157d86fe5b0ad007cb97563c
wonderjeo <455708864@qq.com>
2016-09-24 23:59:27 +0800

    Modified

--- PTA_09.cpp
+++ PTA_09.cpp
@@ -1,4 +1,5 @@
 #include<stdio>
+#include<iostream>
 typedef struct TreeNode* Tree;
 struct TreeNode{
 	int v;
@@ -6,7 +7,41 @@ struct TreeNode{
 	Tree Right;
 	int flag;
 }
+Tree CreateTree(int N);
+Tree CreateTree(int N){
+	int i;
+	int V;
+	Tree T;
+	cin>>V;
+	T=NewNode(V);
+	for(i=0;i<N;i++){
+		cin>>V;
+		Insert(T,V);
+	}
+	return T;
+}
+void Insert(Tree T, int V);
+void Insert(Tree T, int V){
+	if(!Tree)
+}
 int main()
 {
+	int N,L;
+	int i;
+	cin>>N;
+	while(N)
+	{
+		cin>>L;
+		Tree T;
+		T=CreateTree(N);
+		for(i=0;i<L;i++){
+			if(Judge(T,N))
+				cout<<"Yes"<<endl;
+			else
+				cout<<"No"<<endl;
+			Reset(T);
+		}
+		Free(T);
+	}
 	return 0;
 }
