973ade1b00e143a9f862238093408f008ff7e05b
wonderjeo <455708864@qq.com>
2016-09-25 18:04:21 +0800

    Modified

--- PTA_10.cpp
+++ PTA_10.cpp
@@ -1,5 +1,28 @@
-#include<stdio.h>
+#include<iostream>
+typedef struct TreeNode* Tree;
+struct TreeNode{
+	int v;
+	Tree Left,Right;
+	int BF;
+};
+Tree CreateAVLTree(int N);
+Tree CreateAVLTree(int N){
+	Tree T;
+	int i, V;
+	for(i=0;i<N;i++){
+		cin>>V;
+		T=Insert(V);
+	}
+	return T;
+}
+Tree Insert(int V);
+Tree Insert(int V){}
 int main()
 {
+	Tree T;
+	int N;
+	cin>>N;
+	T=CreateAVLTree(N);
+	cout<<T.v;
 	return 0;
 }
